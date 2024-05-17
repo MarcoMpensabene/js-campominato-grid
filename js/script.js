5105c576ffae036d1a814b345778d308a6374a76
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-17 17:43:55 +0200

    added innerthtml on the container , input in htmlfile , if condition on the event so we can change grid size

--- js/script.js
+++ js/script.js
@@ -2,11 +2,24 @@
 const buttonEl = document.querySelector("header button")
 console.log(buttonEl)
 const containerEl = document.querySelector("section.container")
+const selectEl = document.querySelector("header div select")
+let grandezza = selectEl.value
+console.log(selectEl.value)
 buttonEl.addEventListener("click"  , function() {
-    for (let i = 1 ; i <= 100 ; i++){
+    containerEl.innerHTML = "";
+    let grandezza = selectEl.value
+    console.log(grandezza.value)
+for (let i = 1 ; i <= grandezza ; i++){
     const divEl = document.createElement("div")
     containerEl.appendChild(divEl)
     divEl.classList.add("square")
+    if(grandezza > 99 ){
+        divEl.classList.add("large")
+    } else if(grandezza > 80 && grandezza < 100){
+        divEl.classList.add("medium")
+    } else {
+        divEl.classList.add("small")
+    }
     const numbers = document.createElement("span")
     divEl.appendChild(numbers)
     numbers.append(i)
@@ -16,6 +29,6 @@ buttonEl.addEventListener("click"  , function() {
         console.log(i)
 
     },{ once: true });
-}} , { once: true });
+}} );
 
 

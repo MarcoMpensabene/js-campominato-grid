9d060d5de08091410aad2bc74b4f00e85f3eee81
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-17 16:08:05 +0200

    added for cicle , event listner for the button and the square to add tags and class

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const buttonEl = document.querySelector("header button")
+console.log(buttonEl)
+const containerEl = document.querySelector("section.container")
+buttonEl.addEventListener("click" , function() {
+    
+    for (let i = 1 ; i <= 100 ; i++){
+    const divEl = document.createElement("div")
+    containerEl.appendChild(divEl)
+    divEl.classList.add("square")
+    const numbers = document.createElement("span")
+    divEl.appendChild(numbers)
+    numbers.append(i)
+    divEl.addEventListener("click" , function() {
+        divEl.classList.add("active")
+        numbers.classList.add("numbers")
+
+    });
+}});
+
+

fcfc53cae00f4622b649cfc8ba0ad6ad3d3b1062
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-20 10:28:36 +0200

    modified indentation

--- js/script.js
+++ js/script.js
@@ -9,24 +9,24 @@ buttonEl.addEventListener("click"  , function() {
     containerEl.innerHTML = "";
     let grandezza = selectEl.value;
     console.log(grandezza.value);
-for (let i = 1 ; i <= grandezza ; i++){
-    const divEl = document.createElement("div");
-    containerEl.appendChild(divEl);
-    divEl.classList.add("square");
-    if(grandezza > 99 ){
-        divEl.classList.add("large");
-    } else if(grandezza > 80 && grandezza < 100){
-        divEl.classList.add("medium");
-    } else {
-        divEl.classList.add("small");
-    }
-    const numbers = document.createElement("span");
-    divEl.appendChild(numbers);
-    numbers.append(i);
-    divEl.addEventListener("click" , function() {
-        divEl.classList.add("active");
-        numbers.classList.add("numbers");
-        console.log(i)
+    for (let i = 1 ; i <= grandezza ; i++){
+        const divEl = document.createElement("div");
+        containerEl.appendChild(divEl);
+        divEl.classList.add("square");
+        if(grandezza > 99 ){
+            divEl.classList.add("large");
+        } else if(grandezza > 80 && grandezza < 100){
+            divEl.classList.add("medium");
+        } else {
+            divEl.classList.add("small");
+        }
+        const numbers = document.createElement("span");
+        divEl.appendChild(numbers);
+        numbers.append(i);
+        divEl.addEventListener("click" , function() {
+            divEl.classList.add("active");
+            numbers.classList.add("numbers");
+            console.log(i)
 
     },{ once: true });
 }} );
